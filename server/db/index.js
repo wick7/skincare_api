d357111ad70b1398994f27b0bf3ffeba2c047e96
wick7 <wickersham.craig@gmail.com>
2020-08-06 16:40:28 -0700

    added update route, added comments

--- server/db/index.js
+++ server/db/index.js
@@ -13,6 +13,8 @@ const pool = mysql.createPool({
 
 let skincareDB = {};
 
+//GET 
+//http://localhost:3000/api/120
 skincareDB.one = (id) => {
 
     return new Promise((resolve, reject) => {
@@ -76,7 +78,34 @@ skincareDB.insert = (body) => {
             return resolve({ sucessMessage: 'Successfully added entry!' })
         });
     });
+};
 
+//PUT
+//http://localhost:3000/api/update/140
+// {
+//     "category": 10,
+//     "product_name": "Misssss Crehnghamerscc",
+//     "date_purchased": "2020-07-05",
+//     "date_opened": null,
+//     "date_finished": null,
+//     "brand": "Some Guy",
+//     "oz_size": 1,
+//     "quantity": 1,
+//     "price_paid": 5,
+//     "price_per_oz": 5,
+//     "repurchase": 1,
+//     "notes": null
+// }
+skincareDB.update = (id, body) => {
+    let companyId = parseInt(id)
+    return new Promise((resolve, reject) => {
+        pool.query('UPDATE purchased SET category=?, product_name=?, date_purchased=?, date_opened=?, date_finished=?, brand=?, oz_size=?, quantity=?, price_paid=?, price_per_oz=?, repurchase=?, notes=? WHERE id=?;', [body.category, body.product_name, body.date_purchased, body.date_opened, body.date_finished, body.brand, body.oz_size, body.quantity, body.price_paid, body.price_per_oz, body.repurchase, body.notes, companyId], (err, results) => {
+            if (err) {
+                return reject(err)
+            }
+            return resolve(results, { sucessMessage: 'Successfully updated entry!' })
+        });
+    });
 };
 
 module.exports = skincareDB;
