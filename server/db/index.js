62d67d0e41bb73e49bb84475b40adc9418ce6175
wick7 <wickersham.craig@gmail.com>
2020-08-05 22:30:25 -0700

    insert route created -- in progress

--- server/db/index.js
+++ server/db/index.js
@@ -25,6 +25,7 @@ skincareDB.one = (id) => {
     });
 };
 
+//http://localhost:3000/api?search=mas&page=3
 skincareDB.all = (search, page) => {
 
     let firstPage = 0
@@ -42,6 +43,19 @@ skincareDB.all = (search, page) => {
                 return reject(err)
             }
             return resolve(results)
+        });
+    });
+};
+
+//http://localhost:3000/api/insert?category=10&product_name=Miss%20Cream&date_purchased=2020-07-05&date_opened=NULL&date_finished=NULL&brand=Sephora&oz_size=1&quantity=1&price_paid=5&price_per_oz=5&repurchase=1&notes=NULL
+skincareDB.insert = (category, product_name, date_purchased, date_opened, date_finished, brand, oz_size, quantity, price_paid, price_per_oz, repurchase, notes) => {
+    console.log(category, product_name, date_purchased, date_opened, date_finished, brand, oz_size, quantity, price_paid, price_per_oz, repurchase, notes)
+    return new Promise((resolve, reject) => {
+        pool.query('INSERT INTO purchased(category,product_name,date_purchased,date_opened,date_finished,brand,oz_size,quantity,price_paid,price_per_oz,repurchase,notes) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);', [category, product_name, date_purchased, date_opened, date_finished, brand, oz_size, quantity, price_paid, price_per_oz, repurchase, notes], (err, results) => {
+            if (err) {
+                return reject(err)
+            }
+            console.log('1 Record Added')
         });
     });
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -23,4 +23,15 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
+router.post('/insert', (req, res, next) => {
+    console.log(req.query.category, req.query.product_name, req.query.date_purchased, req.query.date_opened, req.query.date_finished, req.query.brand, req.query.oz_size, req.query.quantity, req.query.price_paid, req.query.price_per_oz, req.query.repurchase, req.query.notes)
+    // try {
+    //     let results = await db.insert(req.query.category, req.query.product_name, req.query.date_purchased, req.query.date_opened, req.query.date_finished, req.query.brand, req.query.oz_size, req.query.quantity, req.query.price_paid, req.query.price_per_oz, req.query.repurchase, req.query.notes);
+    //     res.json(results);
+    // } catch (e) {
+    //     console.log(e);
+    //     res.sendStatus(500)
+    // }
+});
+
 module.exports = router;
